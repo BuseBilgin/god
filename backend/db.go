66be6884bd71991d79c63301e1b5fc8b59305680
Buse Gül Rana BİLGİN <136193801+BuseBilgin@users.noreply.github.com>
2025-08-11 16:14:08 +0300

    Update DB config for Railway

--- backend/db.go
+++ backend/db.go
@@ -3,6 +3,9 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -12,16 +15,26 @@ var DB *sql.DB
 func ConnectDB() {
 	var err error
 
-	// ✅ Direkt bağlantı bilgisiyle DSN oluştur
-	dsn := "root:347834@tcp(127.0.0.1:3306)/vize"
+	host := os.Getenv("MYSQLHOST")
+	port := os.Getenv("MYSQLPORT")
+	user := os.Getenv("MYSQLUSER")
+	pass := os.Getenv("MYSQLPASSWORD")
+	name := os.Getenv("MYSQLDATABASE")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&charset=utf8mb4&tls=true",
+		user, pass, host, port, name)
 
 	DB, err = sql.Open("mysql", dsn)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
+	DB.SetMaxOpenConns(10)
+	DB.SetMaxIdleConns(5)
+	DB.SetConnMaxLifetime(time.Hour)
+
 	if err = DB.Ping(); err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	fmt.Println("MySQL bağlantısı başarılı!")
